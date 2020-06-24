6afc6d9e630928a58f562fe04c6572efdbb60538
daltoncorb <daltoncorb@yahoo.com.br>
2020-06-24 16:04:30 -0300

    Apresentação do projeto e demonstração das tags nas página About e sobre controllers

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["Meuemail"] = "Dalton@cubosoft.net.br";
 
             return View();
         }
